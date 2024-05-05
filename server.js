77ea5cce8a1c2aef94954f0001b6e6c4f4869a9a
Mathieu <tatatavu@gmail.com>
2024-05-05 15:53:00 +0200

    test commit

---
+++ server.js
@@ -0,0 +1,12 @@
+//fais moi un petit server express
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
